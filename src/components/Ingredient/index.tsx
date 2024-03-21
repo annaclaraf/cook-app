9141ca4d82b1db3176e4923e3a16c59469053b1b
Anna Clara <anisferraz@gmail.com>
2024-03-20 22:43:37 -0300

    feat: ingredient component

---
+++ src/components/Ingredient/index.tsx
@@ -0,0 +1,18 @@
+import { Image, Pressable, PressableProps, Text } from "react-native";
+
+import { styles } from "./styles";
+
+export type IngredientsProps = {
+  name: string;
+  image: string;
+  selected?: boolean;
+};
+
+export function Ingredient({ name, image, selected = false, ...rest }: IngredientsProps & PressableProps) {
+  return (
+    <Pressable style={[styles.container, selected && styles.selected]} {...rest}>
+      <Image style={styles.image} source={{ uri: image }} />
+      <Text style={styles.title}>{name}</Text>
+    </Pressable>
+  );
+};
